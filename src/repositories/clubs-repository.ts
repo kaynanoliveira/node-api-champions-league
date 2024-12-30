de4efcf1792df58d8a23b0ca927d642885340f3b
kayna <kaynanoliveira357@gmail.com>
2024-12-30 18:19:48 -0300

    commit

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,13 +1,10 @@
 import { ClubModel } from "../models/club-model"
+import fs from "fs/promises"
 
-const database: ClubModel[] = [
-    {
-        id: 1,
-        name: "Real Madrid"
-    }
-]
 
 // Listar todos os clubs 
 export const findAllClub = async ():Promise<ClubModel[]> => {
-    return database
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8")
+    const clubs: ClubModel[] = JSON.parse(data)
+    return clubs
 }
